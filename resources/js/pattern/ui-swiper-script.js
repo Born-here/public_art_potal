f2d98b273504fc2ead1926fd64ce54379d91dfe9
born-here <choi.hyeonjee26@gmail.com>
2024-10-21 00:03:39 +0900

    swiper_slide > text + lottie ani 배치 오류 수정

--- resources/js/pattern/ui-swiper-script.js
+++ resources/js/pattern/ui-swiper-script.js
@@ -1,15 +1,33 @@
 // const progressCircle = document.querySelector('.autoplay-progress svg');
 // const progressContent = document.querySelector('.autoplay-progress span');
-var swiper = new Swiper('.swiper.intro.swiper-container', {
-  spaceBetween: 30,
-  centeredSlides: true,
-  loop: true,
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+// var swiper = new Swiper('.swiper.intro.swiper-container', {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   loop: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
+
+$(document).ready(function () {
+  var swiper = new Swiper('.swiper.intro.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    observer: true,
+    observeParents: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 });
