6a9b1ee433fc990db307af634e6be6a98278f5d0
born-here <choi.hyeonjee26@gmail.com>
2024-10-24 01:16:08 +0900

    1번 페이지  lottiefiles animation 보완

--- resources/js/pattern/ui-swiper-script.js
+++ resources/js/pattern/ui-swiper-script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   var swiper = new Swiper('.swiper_container', {
-    spaceBetween: 30,
+    spaceBetween: 0,
     centeredSlides: true,
     loop: true,
     autoplay: {
-      delay: 99999,
+      delay: 1500,
       disableOnInteraction: false,
     },
     pagination: {
