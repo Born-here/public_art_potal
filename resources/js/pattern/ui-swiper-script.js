397372026747a2c468e2490ca7c956bc7d1ac315
born-here <choi.hyeonjee26@gmail.com>
2024-10-23 15:11:25 +0900

    version 원복

--- resources/js/pattern/ui-swiper-script.js
+++ resources/js/pattern/ui-swiper-script.js
@@ -1,26 +1,10 @@
-// const progressCircle = document.querySelector('.autoplay-progress svg');
-// const progressContent = document.querySelector('.autoplay-progress span');
-// var swiper = new Swiper('.swiper.intro.swiper-container', {
-//   spaceBetween: 30,
-//   centeredSlides: true,
-//   loop: true,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-// });
-
 $(document).ready(function () {
   var swiper = new Swiper('.swiper_container', {
     spaceBetween: 30,
     centeredSlides: true,
     loop: true,
     autoplay: {
-      delay: 2500,
+      delay: 2000,
       disableOnInteraction: false,
     },
     pagination: {
