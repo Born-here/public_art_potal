b115d5c2cade91ea6d2d00ed8121240b11995c76
born-here <choi.hyeonjee26@gmail.com>
2024-10-29 15:37:10 +0900

    daegu section 정렬 진행중

---
+++ resources/js/pattern/ui-gsap-script.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.video-container', {
+  yPercent: -30, // 스크롤에 따라 위로 30% 이동
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.daegu_space',
+    start: 'top top', // 스크롤 시작 위치
+    end: 'bottom top', // 스크롤 끝 위치
+    scrub: true, // 스크롤에 따라 애니메이션 진행
+  },
+});
+
+gsap.to('.daegu_story.index01', {
+  y: -window.innerHeight, // 스크롤에 따라 창 높이만큼 위로 이동
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.daegu_space',
+    start: 'top center', // 트리거 시작 지점
+    end: 'bottom top',
+    scrub: true,
+  },
+});
